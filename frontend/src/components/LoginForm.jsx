f7ccccce00bb694895479e40f0999ee9d30d49aa
mieraf <elaman99619@gmail.com>
2025-11-13 12:13:18 +0300

    add login auth

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import postRequest from '../api/ApiComponents';
+import { Link } from "react-router-dom";
 
 function LoginForm() {
     const [formData, setFormData] = useState({
@@ -8,17 +10,30 @@ function LoginForm() {
 
     const handleChange = (event) => {
         const {name, value} = event.target
-        
+
         setFormData((prevData) => ({
             ...prevData,
             [name]: value
         }))
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
-        console.log('form :' + formData.username)
+        const username = formData.username
+        const password = formData.password
+
+        const res = await postRequest({
+                endPoint: "token",
+                body: {username, password},
+                headers: {'Content-Type': 'application/json'}
+            })
+
+        if (res.status == 200) {
+            console.log(res.data);
+            localStorage.setItem('access', res.data['access'])
+            localStorage.setItem('refresh', res.data['refresh'])
+        }
     }
 
     return (
